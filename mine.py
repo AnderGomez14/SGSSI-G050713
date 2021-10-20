24694312f931299faa6de27e480dc6b48cb0bc98
AnderGomez14 <anderelmejor10@gmail.com>
2021-10-20 15:40:05 +0200

    Añadido argumentos

--- mine.py
+++ mine.py
@@ -4,23 +4,28 @@ import time
 import secrets
 
 
-def tarea1():
-    input = "SGSSI-21.CB.04.txt"
+def mine(input, output, id, numceros):
     with open(input, "rb") as f:
         bytes_file = f.read()
     cont = 0
+    prefijo = "0"*int(numceros)
     while True:
-        hash_i = secrets.token_hex(nbytes=4)
+        hash_i = secrets.token_hex(nbytes=4) + " " + id
         hash = hashlib.sha256(bytes_file + str.encode(hash_i)).hexdigest()
-        if hash.startswith('0000000'):
+        if hash.startswith(prefijo):
             break
         cont += 1
 
-    output = "SGSSI-21.CB.04_mined.txt"
     f = open(output, "wb")
     f.write(bytes_file + str.encode(hash_i))
     f.close()
     print(cont)
 
 
-tarea1()
+if __name__ == '__main__':
+    if(len(sys.argv) == 1 or sys.argv[1] == "-h"):
+        print("Uso: python mine.py archivo_entrada archivo_salida id num_ceros")
+    elif(len(sys.argv) > 4):
+        mine(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    else:
+        print("Numero de argumentos incorrecto")
